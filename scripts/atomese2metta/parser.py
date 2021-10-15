48b6103a5f5ffcd6e3dcb5541a97f8510218e2bc
Jonatas Leon <jonatas.leon@singularity.io>
2021-10-15 09:20:56 -0300

    Add atomese2metta module

---
+++ scripts/atomese2metta/parser.py
@@ -0,0 +1,103 @@
+import os
+import pickle
+
+from io import StringIO, TextIOWrapper
+import re
+import multiprocessing
+from typing import Iterator, Optional, Union
+
+from pyparsing import OneOrMore, nestedExpr
+
+
+class Parser:
+    @staticmethod
+    def _parse(text):
+        return OneOrMore(nestedExpr()).parseString(text).asList()
+
+    def parse(self, text):
+        return self._parse(text)
+
+
+class MultiprocessingParser(Parser):
+    def __init__(self, chunk_size: Optional[int] = None, cpus: Optional[int] = None):
+        self.chunk_size = chunk_size if chunk_size is not None else 1
+        self.cpus = cpus if cpus is not None else multiprocessing.cpu_count()
+        self.manager = multiprocessing.Manager()
+        self.workers_data = {i: self.manager.dict() for i in range(self.cpus)}
+        self._counter_file = 0
+
+    def _next_file_name(self):
+        self._counter_file += 1
+        return f"output_{self._counter_file}.pickle"
+
+    @staticmethod
+    def _count_paren_diff(text: str) -> int:
+        text = re.sub(r"(\".*\")", "", text)
+        return text.count("(") - text.count(")")
+
+    def _split_expressions(self, file: Union[list[str], str, StringIO, TextIOWrapper]) -> Iterator[str]:
+        if isinstance(file, str):
+            file = file.split("\n")
+        
+        counter = 0
+        # TODO: Make expressions a set type.
+        expressions = []
+        expression = ""
+
+        for line in file:
+            if not line:
+                continue
+            counter += self._count_paren_diff(line)
+            expression += line.replace('\n', '')
+            if counter == 0:
+                expressions.append(expression)
+                expression = ""
+
+                if len(expressions) >= self.chunk_size:
+                    yield "".join(expressions)
+                    expressions = []
+
+        if len(expressions) > 0:
+            yield "".join(expressions)
+
+    @staticmethod
+    def _parse(expressions: str, d):
+        d["parser"] = OneOrMore(nestedExpr()).parseString(expressions).asList()
+
+    def _resolve_workers(self, workers) -> str:
+        result = []
+        for worker in workers:
+            worker.join()
+        for _, data in self.workers_data.items():
+            if "parser" in data:
+                result.append(data["parser"])
+
+        filename = self._next_file_name()
+        with open(filename, "wb") as file:
+            pickle.dump(sum(result, start=[]), file)
+        return filename
+
+    def parse(self, file: Union[list[str], str, StringIO, TextIOWrapper]):
+        workers = []
+        result = []
+        for i, expressions in enumerate(self._split_expressions(file)):
+            proc_num = i % self.cpus
+            worker = multiprocessing.Process(
+                target=self._parse,
+                args=(expressions, self.workers_data[proc_num])
+            )
+            workers.append(worker)
+            worker.start()
+
+            if proc_num >= self.cpus - 1:
+                result.append(self._resolve_workers(workers))
+                workers = []
+
+        if len(workers) > 0:
+            result.append(self._resolve_workers(workers))
+
+        data = sum([pickle.load(open(filename, 'rb')) for filename in result], start=[])
+        for filename in result:
+            os.remove(filename) 
+
+        return data
