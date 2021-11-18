09a91d04d8903ea024e02fe2b0f63cc81770fcba
Jonatas Leon <jonatas.leon@singularitynet.io>
2021-11-18 11:30:56 -0300

    Updates hash strategy for Set links

--- scripts/hashing.py
+++ scripts/hashing.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
 from collections import defaultdict
 from hashlib import md5
-from typing import Union
+from typing import Any, Union
+from operator import itemgetter
 
 from atomese2metta.translator import AtomType, Expression, MSet
 
 
+def sort_by_key_hash(keys_hashes: list[Any], keys: list[Any], reverse=False) -> tuple[tuple[Any], ...]:
+    return tuple(zip(*sorted(zip(keys_hashes, keys), key=itemgetter(0), reverse=reverse)))
+
+
 class Hasher:
+
     def __init__(self, algorithm=md5):
         self.algorithm = algorithm
         self.atom_type_dict = dict()
@@ -29,19 +34,20 @@ class Hasher:
 
         return name + atom_type_id
 
-    def get_expression_type_hash(self, expression: Expression) -> str:
+    def get_expression_type_hash(self, types: list[Union[list, str]], salt=None) -> str:
         ids = []
-        for e in expression:
+        for e in types:
             if isinstance(e, Expression):
-                ids.append(self.get_expression_type_hash(e))
+                type_hash = e.type_hash
+                type_hash = (type_hash or self.get_expression_type_hash(e, salt=e.SALT))
+                ids.append(type_hash)
             elif isinstance(e, str):
                 ids.append(self.get_type(e)._id)
             else:
                 raise ValueError(e)
 
-        if isinstance(expression, MSet):
-            ids = sorted(ids)
-            ids.insert(0, expression.SALT)
+        if salt is not None:
+            ids.insert(0, salt)
 
         return self.apply_alg("".join(ids))
 
@@ -50,10 +56,22 @@ class Hasher:
             return self.search_by_name(expression)._id
 
         elif isinstance(expression, Expression):
-            expression_type_hash = self.get_expression_type_hash(expression)
+            if expression._id is not None:
+                return expression._id
+
             keys_hashes = [
                 self.get_expression_hash(key, level=level + 1) for key in expression
             ]
+
+            keys = list(expression)
+            if isinstance(expression, MSet):
+                keys_hashes, keys = sort_by_key_hash(keys_hashes, keys)
+
+            if expression.type_hash is None:
+                expression_type_hash = self.get_expression_type_hash(keys, salt=expression.SALT)
+                expression.type_hash = expression_type_hash
+            else:
+                expression_type_hash = expression.type_hash
 
             signature = expression_type_hash + "".join(keys_hashes)
             hash_id = self.apply_alg(signature)
