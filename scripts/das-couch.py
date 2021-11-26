92f35fc0b65fbff0145cb852247df5d3242b5044
Artur Gontijo <arturgontijo@gmail.com>
2021-11-26 06:43:42 -0300

    [Couchbase] MongoDB read logging - aaaaah.

--- scripts/das-couch.py
+++ scripts/das-couch.py
@@ -50,7 +50,7 @@ def populate_sets(collection: Collection, bucket):
         append(outgoing_set, key=_id, new_value=keys)
         for key in keys:
             append(incoming_set, key=key, new_value=[_id])
-        count += 0
+        count += 1
         if count % 1000 == 0:
             logger.info(f'Documents processed: [{count}/{total}]')
     cursor.close()
