3c8ea0e548a18d807ee69e41cd8d5563bf5883f0
Artur Gontijo <arturgontijo@gmail.com>
2021-12-01 09:33:13 -0300

    [Couchbase] Better initial setup script.

--- scripts/setup/couchbase.sh
+++ scripts/setup/couchbase.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
-is_ready() {
+setup() {
   local ATTEMPTS=$1
-  local URL="localhost:8091/ui/index.html"
-  local OUT=/tmp/is_ready.log
 
   for attempt in $(seq 1 $ATTEMPTS); do
-    status=$(curl -s -w '%{http_code}' -o $OUT $URL)
-    if [ "x$status" == "x200" ]; then
-      echo "Couchbase is ready."
+    echo "INFO: Waiting for Couchbase..."
+    sleep 10
+    docker-compose exec couchbase couchbase-cli \
+      cluster-init \
+      --cluster-name DAS_Cluster \
+      --cluster-username "${DAS_DATABASE_USERNAME}" \
+      --cluster-password "${DAS_DATABASE_PASSWORD}"
+
+    if [ "$?" == 1 ]; then
+      echo "INFO: Couchbase cluster is not ready yet."
+      continue
+    fi
+
+    docker-compose exec couchbase couchbase-cli \
+      bucket-create \
+      -c localhost:8091 \
+      -u "${DAS_DATABASE_USERNAME}" \
+      -p "${DAS_DATABASE_PASSWORD}" \
+      --bucket das \
+      --bucket-type couchbase \
+      --bucket-ramsize "${DAS_COUCHBASE_BUCKET_RAMSIZE}"
+
+    if [ "$?" == 0 ]; then
+      echo "SUCCESS: Couchbase is ready."
       return
     fi
-    echo "Couchbase is starting up..."
-    sleep 5
+
+    echo "INFO: Couchbase is still being set up..."
+
   done
-  echo "Couchbase failed to be set up."
+
+  echo "ERROR: Couchbase failed to be set up."
   return 1
 }
 
-# Wait for Couchbase
-is_ready 5
-
-docker-compose exec couchbase couchbase-cli \
-  cluster-init \
-  --cluster-name DAS_Cluster \
-  --cluster-username "${DAS_DATABASE_USERNAME}" \
-  --cluster-password "${DAS_DATABASE_PASSWORD}"
-
-docker-compose exec couchbase couchbase-cli \
-  bucket-create \
-  -c localhost:8091 \
-  -u "${DAS_DATABASE_USERNAME}" \
-  -p "${DAS_DATABASE_PASSWORD}" \
-  --bucket das \
-  --bucket-type couchbase \
-  --bucket-ramsize "${DAS_COUCHBASE_BUCKET_RAMSIZE}"
+# Couchbase initial setup (attempts=5)
+setup 5
