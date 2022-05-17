b01a949c7a85d33f169ec3fba9789e70290e382f
Andre Senna <andre.senna@gmail.com>
2022-05-17 15:47:09 -0300

    Implementation of NOT expressions

--- pattern_matcher/pattern_matcher_test.py
+++ pattern_matcher/pattern_matcher_test.py
@@ -1,6 +1,7 @@
 import pytest
 from stub_db import StubDB
-from pattern_matcher import PatternMatchingAnswer, LogicalExpression, Variable, Node, Link
+#from pattern_matcher import _AssignmentCompatibilityStatus, _evaluate_compatibility, VariablesAssignment, PatternMatchingAnswer, LogicalExpression, Variable, Node, Link
+from pattern_matcher import VariablesAssignment, PatternMatchingAnswer, LogicalExpression, Variable, Node, Link
 
 
 def test_basic_matching():
@@ -75,6 +76,37 @@ def test_basic_matching():
     assert Link('Set', [ent, Variable('V1'), Variable('V2'), human], False).matched(db, answer)
     assert Link('Set', [monkey, Variable('V1'), Variable('V2'), chimp], False).matched(db, answer)
 
+#def test_evaluate_compatibility():
+#
+#    def build_assignment(d):
+#        answer = VariablesAssignment()
+#        for key in d.keys():
+#            answer.assign(key, d[key])
+#        answer.freeze_assignment()
+#        return answer
+#            
+#    a1 = build_assignment({'v1': 1, 'v2': 2})
+#    a2 = build_assignment({'v1': 1, 'v2': 2})
+#    a3 = build_assignment({'v1': 1, 'v2': 2, 'v3': 3})
+#    a4 = build_assignment({'v1': 1})
+#    a5 = build_assignment({'v3': 3, 'v2': 2})
+#    a6 = build_assignment({'v3': 3, 'v4': 4})
+#    a7 = build_assignment({'v4': 1, 'v5': 2, 'v6': 3})
+#    a8 = build_assignment({'v4': 1, 'v5': 2, 'v1': 3})
+#
+#    assert(_evaluate_compatibility(a1, a2) == _AssignmentCompatibilityStatus.EQUAL)
+#    assert(_evaluate_compatibility(a1, a3) == _AssignmentCompatibilityStatus.SECOND_COVERS_FIRST)
+#    assert(_evaluate_compatibility(a3, a1) == _AssignmentCompatibilityStatus.FIRST_COVERS_SECOND)
+#    assert(_evaluate_compatibility(a4, a2) == _AssignmentCompatibilityStatus.SECOND_COVERS_FIRST)
+#    assert(_evaluate_compatibility(a2, a4) == _AssignmentCompatibilityStatus.FIRST_COVERS_SECOND)
+#    assert(_evaluate_compatibility(a2, a5) == _AssignmentCompatibilityStatus.NO_COVERING)
+#    assert(_evaluate_compatibility(a5, a2) == _AssignmentCompatibilityStatus.NO_COVERING)
+#    assert(_evaluate_compatibility(a2, a6) == _AssignmentCompatibilityStatus.NO_COVERING)
+#    assert(_evaluate_compatibility(a6, a2) == _AssignmentCompatibilityStatus.NO_COVERING)
+#    assert(_evaluate_compatibility(a3, a7) == _AssignmentCompatibilityStatus.NO_COVERING)
+#    assert(_evaluate_compatibility(a3, a8) == _AssignmentCompatibilityStatus.INCOMPATIBLE)
+#    assert(_evaluate_compatibility(a8, a4) == _AssignmentCompatibilityStatus.INCOMPATIBLE)
+    
 
 def test_patterns():
 
@@ -82,8 +114,8 @@ def test_patterns():
         answer: PatternMatchingAnswer = PatternMatchingAnswer()
         assert expected_match == pattern.matched(db, answer)
         if expected_match:
-            assert len(answer.variables_mappings) == len(assignments)
-            l1 = sorted([sorted([f'{x}={y}' for x, y in a.assignment.items()]) for a in answer.variables_mappings])
+            assert len(answer.assignments) == len(assignments)
+            l1 = sorted([sorted([f'{x}={y}' for x, y in a.assignment.items()]) for a in answer.assignments])
             l2 = sorted([sorted([f'{x}={y}' for x, y in d.items()]) for d in assignments])
             print(f'l1 = {l1}\nl2 = {l2}')
             assert l1 == l2
