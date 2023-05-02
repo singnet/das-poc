6f7595526aed3523b78efb7b2f17f3a321154846
Andre Senna <andre.senna@gmail.com>
2023-05-02 15:56:49 -0300

    Cluster setup

--- scripts/redis-up.sh
+++ scripts/redis-up.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-docker stop redis >& /dev/null
-docker rm redis >& /dev/null
+if [ -z "$1" ]
+then
+    PORT=${DAS_REDIS_PORT:-6379}
+else
+    PORT=$1
+fi
+
+echo "Starting Redis on port $PORT"
+
+cp ./redis.conf /tmp/redis_$1.conf
+echo "port $PORT" >> /tmp/redis_$PORT.conf
+
+docker stop redis_$PORT >& /dev/null
+docker rm redis_$PORT >& /dev/null
+
 docker run \
     --detach \
-    --name redis \
+    --name redis_$PORT \
     --env TZ=${TZ} \
-    --env REDIS_ARGS="--port ${DAS_REDIS_PORT:-6379}" \
     --network="host" \
     --volume /tmp:/tmp \
     --volume /mnt:/mnt \
-    redis/redis-stack-server:latest
+    redis/redis-stack-server:latest \
+    redis-server /tmp/redis_$PORT.conf
