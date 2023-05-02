08548b583a89272d8fa45a385555e2a2ae5b1f6f
Andre Senna <andre.senna@gmail.com>
2023-05-02 11:48:14 -0300

    minor changes

--- scripts/mongo-up.sh
+++ scripts/mongo-up.sh
@@ -3,11 +3,12 @@
 docker stop das_mongo_1
 docker rm das_mongo_1
 docker run \
-    --env MONGO_INITDB_ROOT_USERNAME=${DAS_DATABASE_USERNAME:-dbadmin}
-    --env MONGO_INITDB_ROOT_PASSWORD=${DAS_DATABASE_PASSWORD:-dassecret}
-    --env TZ=${TZ}
-    --network="host"
-    --volume /tmp:/tmp
-    --volume /mnt:/mnt
-    --volume mongodbdata:/data/db
+    --env MONGO_INITDB_ROOT_USERNAME=${DAS_DATABASE_USERNAME:-dbadmin} \
+    --env MONGO_INITDB_ROOT_PASSWORD=${DAS_DATABASE_PASSWORD:-dassecret} \
+    --env TZ=${TZ} \
+    --network="host" \
+    --volume /tmp:/tmp \
+    --volume /mnt:/mnt \
+    --volume mongodbdata:/data/db \
+    mongo:latest \
     mongod --port ${DAS_MONGODB_PORT:-27017}
