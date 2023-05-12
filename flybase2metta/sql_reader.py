c637db41b814056e6a3c1da5f67bb7b02c55461d
Andre Senna <andre.senna@gmail.com>
2023-05-12 16:03:22 -0300

    WIP

--- flybase2metta/sql_reader.py
+++ flybase2metta/sql_reader.py
@@ -9,11 +9,13 @@ import sqlparse
 #SQL_LINES_PER_CHUNK = 3000000000
 #SQL_LINES_PER_CHUNK = 3000000
 EXPRESSIONS_PER_CHUNK = 70000000
-SQL_FILE = "/mnt/HD10T/nfs_share/work/datasets/flybase/FB2022_05.sql"
+SQL_FILE = "/mnt/HD10T/nfs_share/work/datasets/flybase/auto_download/2023_02/FB2023_02.sql"
+#SQL_FILE = "/mnt/HD10T/nfs_share/work/datasets/flybase/FB2022_05.sql"
 #SQL_FILE = "/tmp/cut.sql"
 #SQL_FILE = "/tmp/hedra/genes.sql"
 #PRECOMPUTED_DIR = None
-PRECOMPUTED_DIR = "/mnt/HD10T/nfs_share/work/datasets/flybase/precomputed/FB2022_05"
+PRECOMPUTED_DIR = "/mnt/HD10T/nfs_share/work/datasets/flybase/auto_download/2023_02/precomputed"
+#PRECOMPUTED_DIR = "/mnt/HD10T/nfs_share/work/datasets/flybase/precomputed/FB2022_05"
 #PRECOMPUTED_DIR = "/tmp/tsv"
 OUTPUT_DIR = "/mnt/HD10T/nfs_share/work/datasets/flybase_metta"
 #OUTPUT_DIR = "/tmp/flybase"
