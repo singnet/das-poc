15d89924c5c5b61d5761a1c19ef1af49f4ad2365
Andre Senna <andre.senna@gmail.com>
2023-05-29 18:07:47 +0000

    Minor fixes

--- scripts/redis-cluster-start.sh
+++ scripts/redis-cluster-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker exec -it redis_7000 yes yes | redis-cli -p 7000 --cluster create  \
+docker exec -it redis_7000 redis-cli -p 7000 --cluster create  \
     45.32.140.218:7000 \
     45.63.84.83:7000 \
     45.32.141.233:7000 \
     --cluster-replicas 0
+
